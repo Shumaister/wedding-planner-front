e0f60ca78972e756a678527450697959d4c3985b
DigruttolaGabriel <digruttolagabriel@gmail.com>
2023-02-03 00:37:35 -0300

    bootstrap, routing and folders

---
+++ src/components/presentationals/home/home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return(
+        <div className="d-flex flex-column align-items-center mt-3">
+            <h2>Gaby y Nayme</h2>
+        </div>
+    )
+}
+
+export default Home;
